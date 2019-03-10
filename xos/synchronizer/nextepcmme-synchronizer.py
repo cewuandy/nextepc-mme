7c9a385dd886591c856f2e638c49ff216958ff23
Cheng-En Wu <cewuandy@gmail.com>
2019-03-10 21:05:06 +0800

    modify config

--- xos/synchronizer/nextepcmme-synchronizer.py
+++ xos/synchronizer/nextepcmme-synchronizer.py
@@ -16,16 +16,16 @@
 
 # Runs the standard XOS synchronizer
 
+import importlib
 import os
-from xossynchronizer import Synchronizer
+import sys
 from xosconfig import Config
 
-base_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/config.yaml')
-mounted_config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/mounted_config.yaml')
+config_file = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + '/config.yaml')
+Config.init(config_file, 'synchronizer-config-schema.yaml')
 
-if os.path.isfile(mounted_config_file):
-    Config.init(base_config_file, 'synchronizer-config-schema.yaml', mounted_config_file)
-else:
-    Config.init(base_config_file, 'synchronizer-config-schema.yaml')
-
-Synchronizer().run()
+synchronizer_path = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), "../../synchronizers/new_base")
+sys.path.append(synchronizer_path)
+mod = importlib.import_module("xos-synchronizer")
+mod.main()
